88ac85373acf6590b09da9f803b8302de8f1ea4f
Ruslana <ruslana.hural.j@gmail.com>
2025-02-12 17:22:43 +0100

    add thema

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from "react-router-dom"
+import { useState } from "react";
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
+import bg from './assets/images/bg.jpg';
+import bgDark from './assets/images/bg-dark.jpg';
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-[#E8ECD7]">
+    <div className="h-screen w-screen flex justify-center items-center "
+         style={{backgroundImage: `url(${isDarkTheme ? bgDark : bg})`}}>
       <Routes>
-          <Route path='/'  element={<Home />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/'  element={<Home  />} />
+          <Route path='/login' element={<Login thema={isDarkTheme}/>} />
           <Route path='/signup' element={<SignUp />} />
       </Routes>
     </div>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,11 +2,11 @@
 import { Link } from "react-router-dom";
 
 
-function Login() {
+function Login({thema}: {thema:boolean}) {
   return (
-    <div className='flex flex-col items-center justify-center  w-5/12'>
-    <div className='w-full p-6 rounded-[30px] shadow-md bg-[#1F4529] bg-clip-padding'>
-        <h1 className='text-3xl font-black text-center text-[#EED3B1]'>
+    <div className='flex flex-col items-center justify-center w-full  h-full'>
+    <div className={`w-5/12 p-6 rounded-[30px] shadow-md ${thema ? 'bg-[#051206]' : 'bg-[#1A2902]' }`}>
+        <h1 className={`text-3xl font-black text-center ${thema ? 'text-[#614318]' : 'text-[#EED3B1]' }`}>
             Login
             <span className=''> ChatApp</span>
         </h1>
@@ -14,30 +14,33 @@ function Login() {
         <form className="flex flex-col gap-4 pt-10">
             <div >
                 <label className='label'>
-                    <span className='text-white font-bold text-2xl'>Username</span>
+                    <span className={` ${thema ? 'text-[#747575]' : 'text-white'} font-bold text-2xl`}>Username</span>
                 </label>
-                <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10 bg-[#E8ECD7] pl-2 mt-2'  />
+                <input 
+                    type='text' 
+                    placeholder='Enter username' 
+                    className={`w-full input input-bordered h-10 ${thema ? 'bg-[#4e5c4f]' : 'bg-[#E8ECD7]' } pl-2 mt-2`}  />
             </div>
 
             <div>
                 <label className='label'>
-                    <span className='text-white font-bold text-2xl'>Password</span>
+                    <span className={`${thema ? 'text-[#747575]' : 'text-white'}  font-bold text-2xl`}>Password</span>
                 </label>
                 <input
                     type='password'
                     placeholder='Enter Password'
-                    className='w-full input input-bordered h-10 bg-[#E8ECD7] pl-2 mt-2'
+                    className={`w-full input input-bordered h-10 ${thema ? 'bg-[#4e5c4f]' : 'bg-[#E8ECD7]' } pl-2 mt-2 `}
                 />
             </div>
             <Link
                 to='/signup'
-                className='text-sm  hover:underline text-white hover:text-[#EED3B1] text-right mt-2 inline-block'
+                className={`text-sm  hover:underline ${thema ? 'text-[#747575] hover:text-[#614318]' : 'text-white hover:text-[#EED3B1]'}   text-right mt-2 inline-block`}
             >
                 {"Don't"} have an account?
             </Link>
 
             <div className="w-full flex justify-center">
-                <button className='text-[#EED3B1] bg-[#47663B] font-bold py-2 px-4 text-xl  rounded-[10px] '>Login</button>
+                <button className={`${thema ? ' text-[#614318] bg-[#0b2b09]' : 'text-[#EED3B1] bg-[#47663B]'} font-bold py-2 px-4 text-xl  rounded-[10px] `}>Login</button>
             </div>
         </form>
     </div>
