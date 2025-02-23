3c0258ee56d6e052a6276926a684cb469edd22fc
Ruslana <ruslana.hural.j@gmail.com>
2025-02-23 02:46:36 +0100

    add search button

--- frontend/src/components/sideBar/SearchInput.tsx
+++ frontend/src/components/sideBar/SearchInput.tsx
@@ -7,13 +7,13 @@ function SearchInput() {
   const design = useContext(DesignContext);
 
   return (
-    <div className="search-container w-full ">
+    <div className="search-container w-full relative">
         <input 
           type="text"
-          className={`w-full h-8 md:h-10 rounded-lg border-2 px-2 font-medium `} 
+          className={`w-full h-8 md:h-10 rounded-lg border-2 px-2 font-medium  `} 
           style={{backgroundColor: design?.colors.inputColor, borderColor: design?.colors.bgColor, color: design?.colors.bgColor}}
         />
-        <button><Search className='w-4 h-4 md:w-6 md:h-6 outline-none' /></button>
+        <button className="absolute right-2 top-1"><Search className='w-6 h-6 md:w-8 md:h-8' /></button>
       </div>
   )
 }
