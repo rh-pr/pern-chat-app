2eb2adf488b540b0797537c694e49afc87c16db6
Ruslana <ruslana.hural.j@gmail.com>
2025-09-03 22:07:08 +0100

    update base url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const url = import.meta.env.NODE_MODE === "development" ? "http://localhost:5000" : "https://pern-chat-app-npmx.onrender.com";
+// const url = import.meta.env.NODE_MODE === "development" ? "http://localhost:5000" : "https://pern-chat-app-npmx.onrender.com";
 
 const api = axios.create({
-   baseURL: `${url}/api`,
+   baseURL: import.meta.env.VITE_API_URL + '/api',
    withCredentials: true,
 })
 
