81521cf3b797bd19ffb80006fd5355a5fd0c78db
Ruslana <ruslana.hural.j@gmail.com>
2025-09-03 21:18:44 +0100

    update base url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = import.meta.env.NODE_MODE === "development" ? "http://localhost:5000" : "/";
-
+// const url = import.meta.env.NODE_MODE === "development" ? "http://localhost:5000" : "/";
+const url = import.meta.env.BASE_URL;
 const api = axios.create({
    baseURL: `${url}/api`,
    withCredentials: true,
